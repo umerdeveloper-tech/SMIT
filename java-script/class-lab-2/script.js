b399072a55ddc84ba11d7f9681862d44f44210ce
dev_umer01 <umergithub@gmail.com>
2025-02-06 14:51:43 +0500

    Add files via upload

---
+++ java-script/class-lab-2/script.js
@@ -0,0 +1,168 @@
+// let num1 = +prompt('enter a number');
+// let num2 = +prompt ('enter another number');
+// let operator = prompt ('enter operation you want to perform')
+
+// if (operator === '+') {
+//     result = num1 + num2
+// }
+
+// else if (operator === '-') {
+//     result = num1 - num2
+// }
+
+// else if (operator === '*' || operator === 'x') {
+//     result = num1 * num2
+// }
+
+// else if (operator === '/' || operator === '÷') {
+//     result = num1 / num2
+// }
+
+// else if (operator === '%') {
+//     result = num1 % num2
+// }
+
+// else {
+//     result = 'invalid operator'.;
+// }
+
+// document.write {
+//     'result is' + result
+// }
+
+
+// my code!!
+
+
+// let num1 = +prompt('enter a number');
+// let num2 = +prompt('enter another number');
+// let operator = prompt('enter operation you want to perform.');//+-*/%
+// let result;
+
+// if (operator === "+") {
+//     result = num1 + num2;
+// }
+// else if (operator === "-") {
+//     result = num1 - num2;
+// }
+// else if (operator === "x" || operator === "*") {
+//     result = num1 * num2;
+// }
+// else if (operator === "/" || operator === "÷") {
+//     result = num1 / num2;
+// }
+// else if (operator === "%") {
+//     result = num1 % num2;
+// }
+// else {
+//     result = 'Something went wrong.';
+// }
+ 
+
+// console.log (result)
+// document.write("Answer: " + result);
+
+
+
+
+// sir code
+
+
+/*
+
+let marks = +prompt("enter your exam percentage.");
+
+80+       A-1     A+
+70+       A
+60+       B
+50        C
+45        D
+33        E
+33-       F
+
+
+*/
+
+
+// assignment
+
+
+
+// let marks = +prompt ("enter your exam percentage");
+
+// if (marks >= 80) {
+//     document.write("grade:A-1")
+// }
+
+// else if  (marks >= 70 & marks < 80) {
+//     document.write("grade:A")
+// }
+
+// else if  (marks >= 60 & marks < 70) {
+//     document.write("grade:B")
+// }
+
+// else if  (marks >= 50 & marks < 60) {
+//     document.write("grade:C")
+// }
+
+// else if  (marks >= 40 & marks < 50) {
+//     document.write("grade:D")
+// }
+
+// else if  (marks >= 30 & marks < 40) {
+//     document.write("grade:E")
+// }
+
+// else  (marks < 30) {
+//     document.write("grade:fail!!(f)")
+// }
+
+// obtaind / total * 100
+// let marks = +prompt("enter your exam percentage")
+// let total = +prompt ("enter your exam total marks");
+// let obtained  = +prompt ("enter your exam obtained marks")
+// if (total >= 80) {
+    
+// }
+
+// else if  (marks >= 70 & marks < 80) {
+    
+// }
+
+// else if  (marks >= 60 & marks < 70) {
+    
+// }
+
+// else if  (marks >= 50 & marks < 60) {
+    
+// }
+
+// else if  (marks >= 40 & marks < 50) {
+  
+// }
+
+// else if  (marks >= 30 & marks < 40) {
+// }
+
+// else  (marks < 30) && () {
+// }
+
+// else if ()
+
+// document.write("marks: " + grade);
+// document.write("total marks :" + total)
+// document.write("obtaind / total * 100" = percentage)
+
+
+let price = +prompt("enter the price of shirt");
+let price of usb = +prompt("enter the price of usb")
+let quantity = +prompt("enter the quantity of shirt");
+let quantity of usb = +prompt("enter the quantity of usb");
+
+
+let total = price * quantity
+let total of usb = price of usb * quantity of usb
+
+document.write("total : + total")
+document.write("total of usb : + total of usb")
