c80778c976ea85291e510c9d73aaf3d7aea03342
dev_umer01 <umergithub@gmail.com>
2025-02-08 16:44:29 +0500

    Add files via upload

---
+++ java-script/class-3/script.js
@@ -0,0 +1,46 @@
+// nested condition
+
+
+
+
+// let rannum = 10000
+// let score = prompt('what is your score?');
+// let isSpecial = confirm("are you specially abled person");
+
+// if (isSpecial){
+
+//     if (score > 65) {
+//     console.log("your salary is:", rannum * 1.75)} 
+
+//     else if (score <= 55) {
+//     console.log("your salary is:", rannum * 1.25)
+// };
+// };
+
+// let areYouDreaming = confirm("are you dreaming?");
+// if (areYouDreaming) (
+//     console.log("you are dreaming");
+
+// let firstName = "umer";
+// let lastName = "shakil";
+
+// console.log(`my name is ${firstName} ${lastName}`);
+
+
+// let food1 = 500;
+// let food2 = 700;
+// let bill = `your bill is ${food1 + food2}`;
+
+// console.log(bill);
+
+// array start
+
+let fruits = ["apple", "banana", "mango", "orange"];
+console.log(fruits[2]);
+
+
+
+let firstName = "umer";
+let lastName = "shakil";
+
+let fullName = [firstName, lastName];
